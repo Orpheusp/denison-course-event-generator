381ca1f63a6043f1b695029c640a0656c66cb13e
Orpheus Peng <stannolite@gmail.com>
2015-08-21 23:04:41 -0700

    Added basic implementation of the exportCourseEvent function.

--- chrome-extension/generator.js
+++ chrome-extension/generator.js
@@ -1,10 +1,23 @@
 function exportCourseEvent() {
-  var csv = "";
-  window.open('data:text/csv;charset=utf-8,' + escape(csv));
+  // Get the course table.
+  var courseTable = document.getElementsByClassName('datadisplaytable')[0];
+  
+  // Get the headers of the table.
+  var courseTableHeaders = courseTable.getElementsByTagName('th');
+  var output = '';
+  for (var i = 0; i < courseTableHeaders.length; i ++) {
+    output += courseTableHeaders[i].firstChild.nodeValue;
+    output += ', ';
+  }
+  output += '\n';
+  
+  // Get all courses row by row.
+  var courseRows = courseTable.getElementsByTagName('');
+  window.open('data:text/csv;charset=utf-8,' + escape(output));
 }
 
 var btn = document.createElement('BUTTON');
-var text = document.createTextNode("EXPORT CALENDAR");
+var text = document.createTextNode('EXPORT CALENDAR');
 btn.appendChild(text);
-btn.addEventListener("click", exportCourseEvent, false);
+btn.addEventListener('click', exportCourseEvent, false);
 document.getElementsByClassName('pageheaderlinks')[0].appendChild(btn);
