8b6a31a5fba3a121d2cdfe4aa55060367468307a
Orpheus Peng <stannolite@gmail.com>
2015-08-22 16:56:24 -0700

    Modified icons in low resolution.

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -1,17 +1,21 @@
 function loadICalButton() {
-  var exportButton = document.createElement('BUTTON');
-  var exportButtonText = document.createTextNode('EXPORT COURSE ICAL');
-  exportButton.appendChild(exportButtonText);
-  exportButton.addEventListener('click', exportCourseICal, false);
-  document.getElementsByClassName('pageheaderlinks')[0].appendChild(exportButton);
+  if (document.getElementsByClassName('datadisplaytable').length) {
+    var exportButton = document.createElement('BUTTON');
+    var exportButtonText = document.createTextNode('EXPORT COURSE ICAL');
+    exportButton.appendChild(exportButtonText);
+    exportButton.addEventListener('click', exportCourseICal, false);
+    document.getElementsByClassName('pageheaderlinks')[0].appendChild(exportButton);
+  }
 }
 
 function loadCSVButton() {
-  var extractButton = document.createElement('BUTTON');
-  var extractButtontext = document.createTextNode('EXTRACT COURSES');
-  extractButton.appendChild(extractButtontext);
-  extractButton.addEventListener('click', extractCourses, false);
-  document.getElementsByClassName('pageheaderlinks')[0].appendChild(extractButton);
+  if (document.getElementsByClassName('datadisplaytable').length) {
+    var extractButton = document.createElement('BUTTON');
+    var extractButtontext = document.createTextNode('EXTRACT COURSES');
+    extractButton.appendChild(extractButtontext);
+    extractButton.addEventListener('click', extractCourses, false);
+    document.getElementsByClassName('pageheaderlinks')[0].appendChild(extractButton);
+  }
 }
 
 loadCSVButton();
